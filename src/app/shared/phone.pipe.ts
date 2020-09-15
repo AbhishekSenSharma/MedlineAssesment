b4d4b7652da866e6ba40523fdb2d5a770424d06d
AbhishekSenSharma <abhisheksensharma@gmail.com>
2020-09-15 22:20:50 +0530

    Update phone.pipe.ts
    Pipechange

--- src/app/shared/phone.pipe.ts
+++ src/app/shared/phone.pipe.ts
@@ -1,19 +1,11 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
   name: "phone"
 })
-export class PhonePipe {
+export class PhonePipe implements PipeTransform {
   transform(rawNum) {
-    rawNum = rawNum.charAt(0) != 0 ? "0" + rawNum : "" + rawNum;
-
-    let newStr = "";
-    let i = 0;
-
-    for (; i < Math.floor(rawNum.length / 2) - 1; i++) {
-      newStr = newStr + rawNum.substr(i * 2, 2) + " ";
-    }
-
-    return newStr + rawNum.substr(i * 2);
+    const result = (rawNum.slice(0, 3) + ' ' + rawNum.slice(3, 6) + ' ' + rawNum.slice(6, 10));
+    return result;
   }
 }
